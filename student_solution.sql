4b8e93c73f4f23ea46a3f37a2c6d0fa783ba4842
RAMYA S <ramya.s978742@gmail.com>
2026-08-27 10:31:19 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,11 @@
+use school;
+INSERT INTO student(studentName,student id,Gender,DepartmentID)
+VALUES
+('Arun',121 ,'Male', 101),
+('Divya', 122,'Female', 102),
+('Karthik',125,'Male', 103);
+
+SELECT * FROM STUDENT;
+SHOW TABLES;
+DESC STUDENT;
 
